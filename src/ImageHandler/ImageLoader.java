e91c77fcf1ef9ab64702d11ab67596463e32263d
Alex Abramov <37221107+alexabramov2610@users.noreply.github.com>
2018-05-12 22:11:59 +0300

    d

--- src/ImageHandler/ImageLoader.java
+++ src/ImageHandler/ImageLoader.java
@@ -2,9 +2,12 @@ package ImageHandler;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
+/**
+ * loads a picture from user's pc to the game
+ *
+ */
 public class ImageLoader 
 {
 	
